058024ec3b3047bca5e202e42da7b4b33c282941
monircom <monircom@gmail.com>
2024-03-15 23:12:19 +0600

    Banner Added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,6 @@
 import person from '../../assets/images/person.png'
+import banner from '../../assets/images/banner.png'
+
 const Header = () => {
     return (
         <div>      
@@ -56,6 +58,30 @@ const Header = () => {
             </div>
         </nav>
       
+        <div className="max-w-[1170px] mx-auto mb-20">
+                <div className="bg-[#12132D]
+                bg-cover bg-no-repeat rounded-[32px] lg:min-h-[550px]"
+                style={{backgroundImage: `url(${banner})`}}
+                >
+                    <div className="flex flex-col justify-center items-center p-3 m-2">                        
+                        <h1
+                                className="text-white text-3xl  lg:text-[52px]/[76px] text-center font-bold mt-[35px] lg:mt-[130px] mb-6 lg:max-w-[900px]">
+                                Discover an exceptional cooking class tailored for you!
+                            </h1>
+                            <p className="text-white text-center lg:text-lg lg:max-w-[900px] mb-8 mx-3">
+                                Indulge in a personalized culinary adventure with our tailored cooking classes. Whether you're a beginner or a seasoned chef, our expert instructors will guide you through an unforgettable experience.</p>
+    
+                            <div>
+                                <button 
+                                    className="btn bg-[#0BE58A] text-[#150B2B] lg:text-xl px-8 rounded-full mr-6">Explore Now
+                                </button>
+                                <button 
+                                className="btn btn-ghost btn-outline text-white lg:text-xl px-5 rounded-full ">Our Feedback
+                            </button>
+                            </div>  
+                    </div>    
+                </div>
+            </div>
 
    
         </div>
